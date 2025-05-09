053fb99c5b0822c73b248bd786f8a0dad8f69897
Autumn Hoerr <autumn.m.hoerr@gmail.com>
2025-05-08 20:47:05 -0400

    add hostname?

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :autumnhoerr, AutumnhoerrWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  url: [host: "https://autumnhoerr.com"]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Autumnhoerr.Finch
