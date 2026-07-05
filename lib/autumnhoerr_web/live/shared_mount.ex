a016774c947a7b1972f7a501ac8073f1e7b71738
Autumn Hoerr <autumn.m.hoerr@gmail.com>
2026-07-05 10:03:31 -0400

    oops, clean that shit up.

--- lib/autumnhoerr_web/live/shared_mount.ex
+++ lib/autumnhoerr_web/live/shared_mount.ex
@@ -11,7 +11,7 @@ defmodule AutumnhoerrWeb.SharedMount do
 
   import Phoenix.Component
 
-  def on_mount(page_atom, _params, _session, socket) do
+  def on_mount(_, _params, _session, socket) do
     socket =
       socket
       |> assign(
@@ -19,8 +19,7 @@ defmodule AutumnhoerrWeb.SharedMount do
           %{id: "home", label: gettext("Home"), link: ~p"/"},
           %{id: "gallery", label: gettext("Gallery"), link: ~p"/gallery/"},
           %{id: "about", label: gettext("About"), link: ~p"/about/"}
-        ],
-        current_section: Atom.to_string(page_atom)
+        ]
       )
 
     {:cont, socket}
